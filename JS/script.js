be24227c62d792c7286cc2f7526e3e7551a039b1
MrArcano <salvatore-dario@hotmail.it>
2023-09-08 19:21:45 +0200

    [edit]: val var ticket

--- JS/script.js
+++ JS/script.js
@@ -5,73 +5,114 @@ const userTicket = document.getElementById("ticket");
 
 // dichiarazione variabili utili
 let userData = "userData";
-let userKm = 0 ;
-let userAge = 0 ;
+let userKm = 0;
+let userAge = 0;
 let typeTicket = "tipo";
 let trainCarriage = 0;
-let postalCode = "00000" ;
-let price = "00.00€";
+let postalCode = "00000";
+let price = "0";
 let insertOk = false;
+
+// dichiarazione costanti
+const priceKm = 0.21;
+const discountUnder18 = 20;
+const discountOver65 = 40;
 
 btnGen.addEventListener("click", function(){
   // importa i dati dal form
   userData = document.getElementById("user").value;
-  userKm = parseFloat(document.getElementById("km").value);
+  userKm = parseInt(document.getElementById("km").value);
   userAge = parseInt(document.getElementById("age").value);
 
   console.log("carico i dati nelle variabili");
   console.log(userData + " " + userKm + " " + userAge + " ");
   
   // controllo i dati
-  if(userKm > 0 && (userAge >=0 && userAge <=2)){
+  if((userKm > 0) && (userAge >=0 && userAge <=2) && (userData.trim()!="")){
     insertOk=true;
   }else{
-    insertOk=false
-    userTicket.innerHTML = "<h2>Hai inserito dei dati non validi</h2>"
+    insertOk=false;
+    userTicket.innerHTML = "<h2>Hai inserito dei dati non validi</h2>";
   }
 
 
   if(insertOk){
-      // stampo il mio template ticket
-      userTicket.innerHTML +=
-      `
-      <!-- inizio ticket -->
-          <div class="bg-white my-3">
-            <div class="container-fluid p-3">
-              <h2>DETTAGLIO PASSEGGERI</h2>
-              <div class="d-flex justify-content-between">
-        
-                <div class="bg-secondary p-2">
-                  <h4>NOME PASSEGGERO</h4>
-                  <span>${userData}</span>
-                </div>
-        
-                <div class="p-2">
-                  <h5>Offerta</h5>
-                  <span>${typeTicket}</span>
-                </div>
-        
-                <div class="p-2">
-                  <h5>Carrozza</h5>
-                  <span>${trainCarriage}</span>
-                </div>
-        
-                <div class="p-2">
-                  <h5>Codice CP</h5>
-                  <span>${postalCode}</span>
-                </div>
-        
-                <div class="p-2">
-                  <h5>Costo biglietto</h5>
-                  <span>${price}</span>
-                </div>
-        
+    // Calcolo tariffa chilometri
+    price = parseInt(userKm) * priceKm;
+
+    // minorenni
+    if(userAge===0){
+      price = price * (100 - discountUnder18) / 100;
+      typeTicket = "Biglietto Silver";
+    }
+
+    // maggiorenni
+    if(userAge==1){
+      price = price;
+      typeTicket = "Biglietto Normale";
+    }
+
+    // over65
+    if(userAge==2){
+      price = price * (100 - discountOver65) / 100;
+      typeTicket = "Biglietto Gold";
+    }
+
+    // Calcolo numero random carrozza
+    let max = 10;
+    let min = 1;
+    trainCarriage = Math.floor(Math.random() * (max - min + 1) ) + min;
+
+    // Calcolo postalCode
+    max = 98079;
+    min = 10;
+    postalCode = Math.floor(Math.random() * (max - min + 1) ) + min;
+    
+    // stampo il mio template ticket
+    userTicket.innerHTML +=
+    `
+    <!-- inizio ticket -->
+        <div class="bg-white my-3">
+          <div class="container-fluid p-3">
+            <h2>DETTAGLIO PASSEGGERI</h2>
+            <div class="d-flex justify-content-between">
+      
+              <div class="bg-secondary p-2">
+                <h4>NOME PASSEGGERO</h4>
+                <span>${userData}</span>
               </div>
+      
+              <div class="p-2">
+                <h5>Offerta</h5>
+                <span>${typeTicket}</span>
+              </div>
+      
+              <div class="p-2">
+                <h5>Carrozza</h5>
+                <span>${trainCarriage}</span>
+              </div>
+      
+              <div class="p-2">
+                <h5>Codice CP</h5>
+                <span>${postalCode.toString().padStart(5,"0")}</span>
+              </div>
+      
+              <div class="p-2">
+                <h5>Costo biglietto</h5>
+                <span>${parseFloat(price).toFixed(2)} &euro;</span>
+              </div>
+      
             </div>
           </div>
-          <!-- fine ticket -->
-      `;
+        </div>
+        <!-- fine ticket -->
+    `;
   }/* fine if inserimento corretto */
 
   
 })
+
+
+btnDel.addEventListener("click", function(){
+  userTicket.innerHTML = "";
+});
