b1294ff0f8050a74387ff64720da63beaca21750
MrArcano <salvatore-dario@hotmail.it>
2023-09-08 14:00:13 +0200

    [add]: save data form

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,14 @@
-console.log("CIAO");
+
+
+const btnGen = document.getElementById("btn-gen");
+const btnDel = document.getElementById("btn-del");
+
+btnGen.addEventListener("click", function(){
+  // importa i dati dal form
+  const userData = document.getElementById("user").value;
+  const userKm = document.getElementById("km").value;
+  const userAge = document.getElementById("age").value;
+
+
+  console.log(userData + " età: " + userAge + " Km: " + userKm);
+})
